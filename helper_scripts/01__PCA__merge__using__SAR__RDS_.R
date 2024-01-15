69420b9151e41ab5cf391a892b30baa7cac5567f
faraz <fahmed02@icloud.com>
2024-01-15 18:05:54 -0500

    pca extract annotated

---
+++ helper_scripts/01__PCA__merge__using__SAR__RDS_.R
@@ -0,0 +1,58 @@
+#!/usr/bin/env Rscript
+
+library(DESeq2)
+library(SARTools)
+library(dplyr)
+#``````````````````````````````````````````````````````````````````````````````
+# Load in RDS object ====
+args <-  commandArgs(trailingOnly = T)
+
+
+pin <- args[1]
+sar_data <- args[2]
+top_var <- args[3]
+
+
+load(sar_data)
+
+#``````````````````````````````````````````````````````````````````````````````
+# Get VSD Assay ====
+
+vsd <- varianceStabilizingTransformation(out.DESeq2$dds, blind=T)
+
+# calculate the variance for each gene
+rv <- rowVars(assay(vsd))
+
+# select the ntop genes by variance
+select <- order(rv, decreasing=TRUE)[seq_len(min(top_var, length(rv)))]
+
+#``````````````````````````````````````````````````````````````````````````````
+# Get PCA ====
+
+# perform a PCA on the data in assay(vsd) for the selected genes
+pca <- prcomp(t(assay(vsd)[select,]))
+
+#``````````````````````````````````````````````````````````````````````````````
+# Get contribution to the total variance for each component ====
+
+percentVar <- pca$sdev^2 / sum( pca$sdev^2 )
+pVar.df <- as.data.frame(percentVar)
+pVar.df$x = as.factor(paste0("PC",rownames(pVar.df)))
+
+pVar.df = pVar.df[ , order(names(pVar.df))]
+pVar.df$percentVar = pVar.df$percentVar * 100
+pVar.df$percentVar = round(pVar.df$percentVar, digits = 2)
+
+group = target$group
+intgroup.df <- as.data.frame(colData(vsd)[, "group", drop=FALSE])
+
+#``````````````````````````````````````````````````````````````````````````````
+# Set Up PCA DataFrames ====
+df.pca <- data.frame(pca$x, name=rownames(pca$x))
+df.pca.annotated <- left_join(target, df.pca, by= c("label"="name"))
+
+saveRDS(object = df.pca.annotated, file = paste0(pin, "_PCA_Matrix_nTop_", top_var, ".RDS"))
+
+
+
+
